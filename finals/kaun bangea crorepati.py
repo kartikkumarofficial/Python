6d41bd488cd05d09870276efc89fc70247b9b3f8
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-01-06 22:02:32 +0530

    kbc

--- finals/kaun bangea crorepati.py
+++ finals/kaun bangea crorepati.py
@@ -1,3 +1,17 @@
 # create a program capable of displaying questions to the user like KBC
 # use list data type to store questions and their correct answers 
 # display the final amount the person is taking home after playing the game 
+
+
+print("Welcome to Kaun Banega Crorepati")
+questions = ["How many continents are there?", "What is the capital of India?", "NG mei kaun se course padhaya jaata hai?"]
+options = [["1.Four", "2.Nine", "3.Seven", "4.Eight"], 
+           ["1.Chandigarh", "2.Bhopal", "3.Chennai", "4.Delhi"], 
+           ["1.Software Engineering", "2.Counseling", "3.Tourism", "4.Agriculture"]]
+
+
+
+
+
+
+
--- kaun bangea crorepati.py
+++
@@ -1,3 +0,0 @@
-# create a program capable of displaying questions to the user like KBC
-# use list data type to store questions and their correct answers 
-# display the final amount the person is taking home after playing the game 
