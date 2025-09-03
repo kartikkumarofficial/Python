4b077d22660a95ff1b92de6d90a047507e8525a0
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-09-04 00:19:27 +0530

    python

---
+++ libraries/pandass/csv_manipulation.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('mercedesbenz.csv')
+print(df.head())
+df.info()
--- libraries/pandass/dataframe.py
+++ libraries/pandass/dataframe.py
@@ -7,6 +7,9 @@ df =pd.DataFrame(np.arange(0,20).reshape(5,4),index=['R1','R2','R3','R4','R5'],c
 # to export this as csv file
 # df.to_csv('t.csv')
 
-print(df.loc['R1'])
-print(type(df['C1']))
+# print(df.loc[['R1','R3'],['C1','C2']])
+# print(type(df['C1']))
+# print(df.isnull().sum())
+
+print(type(df[['C1','C2']]))
 
